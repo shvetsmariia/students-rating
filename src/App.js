0ecf6462da616fbe23d957740c44ab958a4727fe
Mariia <shvetsmariia@yahoo.com>
2023-06-15 19:01:48 -0500

    initial commit: students rating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [students, setStudents] = useState([
+    {
+      name: 'Андрій',
+      score: 200
+    },
+    {
+      name: 'Світлана',
+      score: 100
+    },
+    {
+      name: 'Людмила',
+      score: 200
+    },
+    {
+      name: 'Іван',
+      score: 200
+    }
+]); 
+  const [averageScore, setAvegareScore] = useState(175); 
+
+  function handleChange(event, index) {
+    const updatedStudents = [...students];
+    updatedStudents[index].score = Number(event.target.value); 
+    updatedStudents.sort((a, b) => b.score - a.score);
+
+    const totalScores = updatedStudents.reduce((total, student) => total + student.score, 0);
+    const average = totalScores / updatedStudents.length;
+
+    setStudents(updatedStudents);
+    setAvegareScore(average);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '20px'}}>
+      <h1>Рейтинг студентів</h1>
+      <h2>Середній бал: {averageScore} балів</h2>
+      <table>
+          {students.map((student, i) => (
+            <tr key={i}>
+              <td>{student.name}</td>
+              <td><input type="text" value={student.score} onChange={(event) => handleChange(event, i)}></input></td> 
+            </tr>
+          ))}
+      </table>
     </div>
   );
 }
 
-export default App;
+export default App; 
